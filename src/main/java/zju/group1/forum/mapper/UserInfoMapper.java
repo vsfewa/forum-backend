5f86a1253cd238b376a996fbcb7b6d82bc0067d7
zjucsq <1064359985@qq.com>
2020-04-29 14:10:16 +0800

    v 2.7.20

--- src/main/java/zju/group1/forum/mapper/UserInfoMapper.java
+++ src/main/java/zju/group1/forum/mapper/UserInfoMapper.java
@@ -18,11 +18,11 @@ public interface UserInfoMapper {
     @Select("select * from user_info where email=#{email}")
     UserInfo getUserInfo(String email);
 
-    @Update("update user_info set real_name = #{real_name}" +
-            "real_name_hidden = #{real_name_hidden}, birth = #{birth}, birth_hidden = #{birth_hidden}" +
-            "phone = #{phone}, phone_hidden = #{phone_hidden}, gender = #{gender}" +
-            "gender_hidden = #{gender_hidden}, hometown = #{hometown}, hometown_hidden = #{hometown_hidden}" +
-            "organization = #{organization}, organization_hidden = #{organization_hidden}" +
+    @Update("update user_info set real_name = #{real_name}, " +
+            "real_name_hidden = #{real_name_hidden}, birth = #{birth}, birth_hidden = #{birth_hidden}, " +
+            "phone = #{phone}, phone_hidden = #{phone_hidden}, gender = #{gender}, " +
+            "gender_hidden = #{gender_hidden}, hometown = #{hometown}, hometown_hidden = #{hometown_hidden}, " +
+            "organization = #{organization}, organization_hidden = #{organization_hidden}, " +
             "signature = #{signature} where email = #{email}")
     void updateUserInfo(UserInfo info);
 }
