ce55435fd7d427097e13983d350a2ce8a9fc60e4
sfofgalaxy <3170105860@zju.edu.cn>
2020-05-06 22:34:19 +0800

    version 3.0

--- src/main/java/zju/group1/forum/dto/Posting.java
+++ src/main/java/zju/group1/forum/dto/Posting.java
@@ -13,11 +13,6 @@ public class Posting {
     private String title;
     private int type;
     private String content;
-//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-//    @JsonFormat(
-//            pattern = "yyyy-MM-dd HH:mm:ss",
-//            timezone = "GMT+8"
-//    )
     private String time;
     private int replyN;
     private int visitN;
--- src/main/java/zju/group1/forum/dto/PostingMessage.java
+++ src/main/java/zju/group1/forum/dto/PostingMessage.java
@@ -15,11 +15,6 @@ public class PostingMessage {
     private String author;
     private String title;
     private String content;
-//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-//    @JsonFormat(
-//            pattern = "yyyy-MM-dd HH:mm:ss",
-//            timezone = "GMT+8"
-//    )
     private String time;
     private int replyN;
     private int visitN;
